257505cb2f723f44fc63571259b151669003621e
Louis Ruocco <louisruocco1@gmail.com>
2023-06-27 21:05:54 +0100

    able to set image as lock screen

--- script.ps1
+++ script.ps1
@@ -1,11 +1,11 @@
 $uri = "https://api.nasa.gov/planetary/apod?api_key=V2PQDwflXvkWutaSwawN6jxuikObYO96Wam7ORjo "
 $req = Invoke-RestMethod $uri
-$req
 
 $imgURL = $req.hdurl
-$imgURL
 $date = (Get-Date).Date.ToString("yyyyMMdd")
-$date
 $dest = "E:\Photos\Space Images\$date.png"
 
 Invoke-WebRequest $imgURL -OutFile $dest
+
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP"
+New-ItemProperty -Path $RegPath -Name LockScreenImagePath -Value $dest -PropertyType String -Force | Out-Null
